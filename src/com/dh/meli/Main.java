16ab0f18779870de0f7e5d773e57501e0f6a3c2e
MauriKlein <maurijklein@gmail.com.br>
2021-08-25 10:04:31 -0300

    Completando o código.

--- src/com/dh/meli/Main.java
+++ src/com/dh/meli/Main.java
@@ -1,8 +1,36 @@
 package com.dh.meli;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        List<Roupa> roupasNobre = new ArrayList<>();
+        List<Roupa> roupasPedro = new ArrayList<>();
+
+        roupasNobre.add(new Roupa("Nike", "Calção"));
+        roupasNobre.add(new Roupa("Nike", "camiseta"));
+
+        roupasPedro.add(new Roupa("umbro", "jaqueta"));
+        roupasPedro.add(new Roupa("umbro", "calça"));
+
+        GuardaRoupa guardaRoupa = new GuardaRoupa();
+
+        guardaRoupa.guardarRoupas(roupasNobre);
+        guardaRoupa.guardarRoupas(roupasPedro);
+
+        guardaRoupa.mostrarRoupas();
+
+        guardaRoupa.devolverRoupas(1);
+
+        System.out.println("****************Depois de Remover***************");
+
+        guardaRoupa.mostrarRoupas();
+
+
+
+
     }
 }
